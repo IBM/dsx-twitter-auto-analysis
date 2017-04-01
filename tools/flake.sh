d631fda64c3a6dc8fc67755de1c99634f340b0aa
Mark Sturdevant <mark.sturdevant@ibm.com>
2017-03-31 20:21:28 -0700

    Add flake.sh script tool for finding pep8 problems
    This simple script is similar to the CI command which
    converts the notebook and runs flake8. Since the line
    numbers for errors don't match notebook cells lines,
    having this tool to look for the lines in the output
    file can be helpful.

--- tools/flake.sh
+++ tools/flake.sh
@@ -5,8 +5,15 @@
 #
 # This script is useful to figure out CI pep8 errors. Use this and the
 # flake.out file to understand the line numbers.
+#
+# params: $1 the notebook(s) to process
 
-jupyter nbconvert notebooks/*.ipynb --stdout --to script | sed 's/^get_ipython.*$/# noqa/' > flake.out
+if [ -z "$1" ]; then
+   echo "Usage: flake.sh <notebook(s)>"
+   exit 1
+fi
+
+jupyter nbconvert $1 --stdout --to script | sed 's/^get_ipython.*$/# noqa/' > flake.out
 
 flake8 --ignore=F401,F404,E402,E703,E712,W291,W293 flake.out
 
